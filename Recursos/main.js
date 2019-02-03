506208206150fff87ab546813fa0f4f63b1598db
Alexanderuser.name <alexander.hortua10@gmail.com>
2019-02-02 16:19:19 -0800

    adding jquery

--- Recursos/main.js
+++ Recursos/main.js
@@ -26,9 +26,34 @@ $('#peace').click(function(){
 $('#sad').click(function(){
 
     $(':root').css('--brand-color','#3498db');
-    $('.wrapper-header').css('background-image','url(Recursos/css/imagenes/sad.jpg)')
+    $('.wrapper-header').css('background-image','url(Recursos/css/imagenes/sad.jpg)');
+   
 })
 
 
+
+
+
+//toggle the mood menu at the top
+$('.mood-menu').click(function(){
+
+    $('.mood').toggle();
+    });
+
+$('#choseMode').click(function(){
+
+    $('.mood').toggle();
+    });
+
+
+//move the screen depending in the position 
+
+
+
+$('.arrow-down').click(function(){
+
+    $("HTML, BODY").animate({ scrollTop: 750 }, 1000); });
     
+
+
 });
