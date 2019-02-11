bdbb9b3f7de2b2bb8775a33426b42068ee716b4e
Alexanderuser.name <alexander.hortua10@gmail.com>
2019-02-10 22:47:54 -0800

    adding new skills, statistics

--- Recursos/Js/galatic.js
+++ Recursos/Js/galatic.js
@@ -60,20 +60,21 @@ class Planet {
     let Inhabitent = 10000+Math.ceil(Math.random()*20000000);
     let Army = Math.ceil(Inhabitent*0.01);
     let planetlogo = Math.ceil(Math.random()*planetas.length-1);
+    let shiplogo = Math.ceil(Math.random()*battleShips.length-1);
 
     
     if(i%2==0){
 
         //creates a planet
        planets[empire_choosen+i] = new Planet('Planet '+i,empire_choosen,Inhabitent,Army,'Earth',position_width,position_height);
-       $('.battleship').append(`<div rel="${empire_choosen+i}" class="ships planet ${empire_choosen} select-planet" style="top:${position_height}%; left:${position_width}%;">${planetas[planetlogo]}</div>`);
+       $('.battleship').append(`<div rel="${empire_choosen+i}" class="ships planet ${empire_choosen} select-planet" style="top:${position_height}%; left:${position_width}%;"><img src="Recursos/css/imagenes/SVG/Planets/planets/planet${planetas[planetlogo]}.svg" width="2"></div>`);
 
         
     }else{
          
         //creates a ship
     ships[empire_choosen+i] = new Spaceship('Ship '+i,empire_choosen,crew, battlePower,EnergyBattle,position_width,position_height);
-    $('.battleship').append(`<div rel="${empire_choosen+i}" class="ships ship select-ship ${empire_choosen}" style="top:${position_height}%; color:${(kingdoms[empire_choosen].color)}; left:${position_width}%; animation: raining;">${(kingdoms[empire_choosen].ship)}</i></div>`)
+    $('.battleship').append(`<div rel="${empire_choosen+i}" class="ships ship select-ship ${empire_choosen}" style="top:${position_height}%; color:${(kingdoms[empire_choosen].color)}; left:${position_width}%; animation: raining;"><img src="Recursos/css/imagenes/SVG/Planets/Ships/spaceship${battleShips[shiplogo]}.svg" width="2"></div>`)
     }
     
 } // end for
@@ -85,7 +86,7 @@ class Planet {
 
 $('.select-ship').hover(function(){
     let information = $(this)[0].attributes[0].nodeValue;
-    console.log(ships[information]);
+    
     $('.box-info').css({'display':'block', 'top': ships[information].positionY+1+'%','left':ships[information].positionX+1+'%'})
     $('.box-info-title').html(`<h4><span>Name: </span>${ships[information].name}</h4>`);
     $('.box-info-resumen').html(`<h6><span>Kingdom: </span>${ships[information].kingdom}</h6> <h6><span>Crew: </span>${ships[information].crew}</h6> <h6><span>Battle Power: </span>${ships[information].battlePower}</h6><h6><span>PositionX: </span>${ships[information].positionX}</h6><h6><span>PositionY: </span>${ships[information].positionY}</h6>`)
@@ -96,7 +97,7 @@ $('.select-ship').hover(function(){
 
 $('.select-planet').hover(function(){
     let information = $(this)[0].attributes[0].nodeValue;
-    console.log(planets[information]);
+ 
     $('.box-info').css({'display':'block', 'top': planets[information].positionY+1+'%','left':planets[information].positionX+1+'%'})
     $('.box-info-title').html(`<h4><span>Name: </span>${planets[information].name}</h4>`);
     $('.box-info-resumen').html(`<h6><span>Kingdom: </span>${planets[information].kingdom}</h6> <h6><span>Inhabitents: </span>${planets[information].crew}</h6> <h6><span>Combat Army: </span>${planets[information].battlePower}</h6><h6><span>Composition: </span>${planets[information].composition}</h6><h6><span>PositionX: </span>${planets[information].positionX}</h6><h6><span>PositionY: </span>${planets[information].positionY}</h6>`)
@@ -108,9 +109,14 @@ $('.select-planet').hover(function(){
 
 Object.keys(kingdoms).forEach(imperio =>{
 let imperioPlanets = 0;
+let totalPlanets = 0;
 let imperioInhabitents = 0;
+let totalInhabitents = 0;
 let shipsKingdom = 0;
+let totalShips = 0;
 for (var property1 in planets) {
+    totalPlanets++;
+    totalInhabitents+= planets[property1].crew;
     if(planets[property1].kingdom==imperio){
         imperioInhabitents+= planets[property1].crew;
         imperioPlanets ++;
@@ -118,20 +124,28 @@ for (var property1 in planets) {
   }
 
   for (var property2 in ships) {
+      totalShips++;
     if(ships[property2].kingdom==imperio){
         shipsKingdom ++;
     };
   }
 
-  //adding info to the object;
+  //adding info to the object kingdoms;
 kingdoms[imperio].planets = imperioPlanets;
-kingdoms[imperio].Inhabitants = Math.ceil(imperioInhabitents/1000000) +" millions";
-kingdoms[imperio].Army =Math.ceil(imperioInhabitents/10000000) +" FKA";
-kingdoms[imperio].NumberShips = shipsKingdom;
+kingdoms[imperio].Inhabitants = Math.ceil(imperioInhabitents/1000000);
+kingdoms[imperio].Army =Math.ceil(imperioInhabitents/10000000);
+kingdoms[imperio].ships = shipsKingdom;
+
+//adding info to the object GLOBAL STATISTICS;
+
+GLOBAL_STATISTICS.planets = totalPlanets;
+GLOBAL_STATISTICS.ships=totalShips;
+GLOBAL_STATISTICS.Inhabitants= Math.ceil(totalInhabitents/1000000);
+GLOBAL_STATISTICS.Army =  GLOBAL_STATISTICS.Inhabitants/10;
 })
 
-//fetching the data to the screen
-console.log(kingdoms);
+//fetching the cards data to the screen
+
 
 Object.keys(kingdoms).forEach(king => {
 
@@ -142,10 +156,10 @@ Object.keys(kingdoms).forEach(king => {
             ${kingdoms[king].logo}
         </div>
         <div class="kingdom-facts">
-            <i class="fas fa-rocket"> ${kingdoms[king].NumberShips}</i>
+            <i class="fas fa-rocket"> ${kingdoms[king].ships}</i>
             <i class="fas fa-globe-americas"> ${kingdoms[king].planets}</i><br>
-            <i class="fas fa-fist-raised"> ${kingdoms[king].Army}</i>
-            <i class="fas fa-users"> ${kingdoms[king].Inhabitants}</i>
+            <i class="fas fa-fist-raised"> ${kingdoms[king].Army} K</i>
+            <i class="fas fa-users"> ${kingdoms[king].Inhabitants} MM</i>
         </div>
     </div>
     <a rel="${kingdoms[king].name.toLowerCase()}" class="selector-kingdom">Show Kingdom</a>
@@ -154,14 +168,53 @@ Object.keys(kingdoms).forEach(king => {
 
 });
 
-
-
+//this part will show the user the whole universe or just the selected kingdom
+let show_universe = 0; 
 $('.selector-kingdom').click(function(){
-   let selection = $(this)[0].attributes[0].nodeValue;
-   $('.ships').hide(2000);
-   $('.'+selection).show(1000);
-   $('.selector-kingdom').html('Show Kingdom')
-   $(this).html("Show Universe");
+ 
+if (show_universe==0){let selection = $(this)[0].attributes[0].nodeValue;
+    $('.selector-kingdom').html('Show Kingdom')
+    $(this).html("Show Universe");
+    $('.selector-kingdom').css({"background":"transparent", 'color':'white'});
+    $(this).css({"background":"greenyellow", 'color':'black'});
+    $('.ships').hide(2000);
+    $('.'+selection).show(1000);
+    show_universe = 1;
+}
+    else{
+        $('.ships').show(1000);
+        $(this).html("Show Kingdom");
+        $(this).css({"background":"transparent", 'color':'white'});
+        show_universe = 0;
+    }
+   
 })
 
+
+//adding the statistics area: 
+
+let statistics_Group = ['planets','ships','Army'];
+$('.full-stadistics-container').html("");
+let statistics_string ="";
+
+statistics_Group.forEach(sGroup=>{
+
+    statistics_string+= `<div class="stats">
+<h3>${sGroup}</h3>`;
+
+    Object.keys(kingdoms).forEach(king => {
+       statistics_string+= `
+<div class="Statistics-bar statistics-planet-Empire">
+    <span>${kingdoms[king].name}</span>
+    <div class="Statistics-bar-full">
+        <div style="width:${(kingdoms[king][sGroup]/GLOBAL_STATISTICS[sGroup])*100+20}%">${kingdoms[king][sGroup]} (${Math.ceil((kingdoms[king][sGroup]/GLOBAL_STATISTICS[sGroup])*100)}%)</div>
+    </div>
+</div>`});
+statistics_string+=`</div>`;
+})
+
+$('.full-stadistics-container').append(statistics_string);
+
+
+console.log(GLOBAL_STATISTICS);
 });
--- Recursos/Js/objects.js
+++ Recursos/Js/objects.js
@@ -1,132 +1,118 @@
 
-    quotes = {'cheerful':[{author:"—George Matthew Adams",quotes:"I say to myself that I shall try to make my life like an open fireplace, so that people may be warmed and cheered by it and so go out themselves to warm and cheer."},
-    {author:"—Orison Swett Marden",quotes:"It is the hopeful, buoyant, cheerful attitude of mind that wins. Optimism is a success builder, pessimism an achievement killer."},
-    {author:" —Jimmy Buffett",quotes:"It takes no more time to see the good side of life than to see the bad."},
-    {author:" —Jean Paul",quotes:" Laughing cheerfulness throws the light of day on all the paths of life."},
-    {author:" —William Rotsler",quotes:" You cannot hold back a good laugh any more than you can the tide. Both are forces of nature. "},
-    {author:" —Helen Keller",quotes:" The unselfish effort to bring cheer to others will be the beginning of a happier life for ourselves."},
-    {author:" —Mark Twain",quotes:" The best way to cheer yourself up is to try to cheer somebody else up."},
-    {author:" —Michel de Montaigne",quotes:" The most certain sign of wisdom is cheerfulness."},
-    {author:" —Charles Dickens",quotes:" Cheerfulness and contentment are great beautifiers, and are famous preservers of good looks. "},
-    {author:" —Ralph Waldo Emerson",quotes:" And so of cheerfulness, or a good temper—the more it is spent, the more of it remains."},
-    
-    ],
-    
-    'madness':[{author:"— Friedrich Nietzsche",quotes:" There is always some madness in love. But there is also always some reason in madness."},
-    {author:"— Aristotle",quotes:" No great mind has ever existed without a touch of madness."},
-    {author:" —Kahlil Gibran:", quotes: "I have found both freedom and safety in my madness; the freedom of loneliness and the safety from being understood, for those who understand us enslave something in us."},
-    {author:" —Yann Martel, Life of Pi", quotes: "All living things contain a measure of madness that moves them in strange, sometimes inexplicable ways. This madness can be saving; it is part and parcel of the ability to adapt. Without it, no species would survive."},
-    {author:" —Charles Bukowski, Ham on Rye",quotes:" I had noticed that both in the very poor and very rich extremes of society the mad were often allowed to mingle freely."},
-    {author:" — Wei Hui",quotes:" Crazy people are considered mad by the rest of the society only because their intelligence isn't understood."},
-    {author:" — Roman Payne",quotes:" All forms of madness, bizarre habits, awkwardness in society, general clumsiness, are justified in the person who creates good art."},
-    {author:" — Thomas Moore",quotes:" It is only through mystery and madness that the soul is revealed."},
-    {author:" — Charles Bukowski",quotes:" Understand me. I’m not like an ordinary world. I have my madness, I live in another dimension and I do not have time for things that have no soul."},
-    {author:" — Friedrich Nietzsche,",quotes:" Madness is something rare in individuals — but in groups, parties, peoples, and ages, it is the rule."}
-    ],
-    
-    
-    'Peaceful':[{author:"— Bernie Siegel",quotes:" Love and peace of mind do protect us. They allow us to overcome the problems that life hands us. They teach us to survive… to live now… to have the courage to confront each day."},
-    {author:"— George Michael",quotes:" You’ll never find peace of mind until you listen to your heart."},
-    {author:" —Dalai Lama", quotes: "Do not let the behavior of others destroy your inner peace."},
-    {author:" —Eckhart Tolle ",quotes: "You find peace not by rearranging the circumstances of your life, but by realizing who you are at the deepest level."},
-    {author:" —John F. Kennedy",quotes:" Peace is a daily, a weekly, a monthly process, gradually changing opinions, slowly eroding old barriers, quietly building new structures."},
-    {author:" — Linda Evans",quotes:" If there’s no inner peace, people can’t give it to you. The husband can’t give it to you. Your children can’t give it to you. You have to give it to you."},
-    {author:" — Haile Selassie",quotes:" Peace is a day-to-day problem, the product of a multitude of events and judgments. Peace is not an ‘is,’ it is a ‘becoming.’"},
-    {author:" — Eleanor Roosevelt",quotes:" It isn’t enough to talk about peace. One must believe in it. And it isn’t enough to believe in it. One must work at it."},
-    {author:" — Ralph Waldo Emerson",quotes:" Nobody can bring you peace but yourself."},
-    {author:" — Norman Vincent Peale", quotes:" The life of inner peace, being harmonious and without stress, is the easiest type of existence."}
-    ],
-    
-    
-    'Melancholy':[{author:"— Daniel Keyes",quotes:"I don’t know what’s worse: to not know what you are and be happy, or to become what you’ve always wanted to be, and feel alone."},
-    {author:"— Soren Kierkegaard",quotes:" What if everything in the world were a misunderstanding, what if laughter were really tears?"},
-    {author:" —Susan Sontag", quotes: "Depression is melancholy minus its charms."},
-    {author:" —P.G. Wodehouse",quotes: "A melancholy-looking man, he had the appearance of one who has searched for the leak in life's gas-pipe with a lighted candle."},
-    {author:" —Percy Bysshe Shelley",quotes:" A poet is a nightingale who sits in darkness and sings to cheer its own solitude with sweet sounds."},
-    {author:" — Sylvia Plath",quotes:" I wish to cry. Yet, I laugh, and my lipstick leaves a red stain like a bloody crescent moon on the top of the beer can."},
-    {author:" — Hermann Hesse",quotes:" I began to understand that suffering and disappointments and melancholy are there not to vex us or cheapen us or deprive us of our dignity but to mature and transfigure us."},
-    {author:" — Tim Winton",quotes:" It’s how I fill the time when nothing’s happening. Thinking too much, flirting with melancholy."},
-    {author:" — William Shakespeare",quotes:" There's little of the melancholy element in her, my lord: she is never sad but when she sleeps; and not ever sad then; for I have heard my daughter say, she hath often dreamt of unhappiness, and waked herself with laughing."},
-    {author:" — Oscar Wilde ", quotes: "The gods are strange. It is not our vices only they make instruments to scourge us. They bring us to ruin through what in us is good, gentle, humane, loving"}
-    ]
-    
-    
-    }
-    
+quotes = {
+    'cheerful': [{ author: "—George Matthew Adams", quotes: "I say to myself that I shall try to make my life like an open fireplace, so that people may be warmed and cheered by it and so go out themselves to warm and cheer." },
+    { author: "—Orison Swett Marden", quotes: "It is the hopeful, buoyant, cheerful attitude of mind that wins. Optimism is a success builder, pessimism an achievement killer." },
+    { author: " —Jimmy Buffett", quotes: "It takes no more time to see the good side of life than to see the bad." },
+    { author: " —Jean Paul", quotes: " Laughing cheerfulness throws the light of day on all the paths of life." },
+    { author: " —William Rotsler", quotes: " You cannot hold back a good laugh any more than you can the tide. Both are forces of nature. " },
+    { author: " —Helen Keller", quotes: " The unselfish effort to bring cheer to others will be the beginning of a happier life for ourselves." },
+    { author: " —Mark Twain", quotes: " The best way to cheer yourself up is to try to cheer somebody else up." },
+    { author: " —Michel de Montaigne", quotes: " The most certain sign of wisdom is cheerfulness." },
+    { author: " —Charles Dickens", quotes: " Cheerfulness and contentment are great beautifiers, and are famous preservers of good looks. " },
+    { author: " —Ralph Waldo Emerson", quotes: " And so of cheerfulness, or a good temper—the more it is spent, the more of it remains." },
 
-    const kingdoms = {
+    ],
+
+    'madness': [{ author: "— Friedrich Nietzsche", quotes: " There is always some madness in love. But there is also always some reason in madness." },
+    { author: "— Aristotle", quotes: " No great mind has ever existed without a touch of madness." },
+    { author: " —Kahlil Gibran:", quotes: "I have found both freedom and safety in my madness; the freedom of loneliness and the safety from being understood, for those who understand us enslave something in us." },
+    { author: " —Yann Martel, Life of Pi", quotes: "All living things contain a measure of madness that moves them in strange, sometimes inexplicable ways. This madness can be saving; it is part and parcel of the ability to adapt. Without it, no species would survive." },
+    { author: " —Charles Bukowski, Ham on Rye", quotes: " I had noticed that both in the very poor and very rich extremes of society the mad were often allowed to mingle freely." },
+    { author: " — Wei Hui", quotes: " Crazy people are considered mad by the rest of the society only because their intelligence isn't understood." },
+    { author: " — Roman Payne", quotes: " All forms of madness, bizarre habits, awkwardness in society, general clumsiness, are justified in the person who creates good art." },
+    { author: " — Thomas Moore", quotes: " It is only through mystery and madness that the soul is revealed." },
+    { author: " — Charles Bukowski", quotes: " Understand me. I’m not like an ordinary world. I have my madness, I live in another dimension and I do not have time for things that have no soul." },
+    { author: " — Friedrich Nietzsche,", quotes: " Madness is something rare in individuals — but in groups, parties, peoples, and ages, it is the rule." }
+    ],
+
+
+    'Peaceful': [{ author: "— Bernie Siegel", quotes: " Love and peace of mind do protect us. They allow us to overcome the problems that life hands us. They teach us to survive… to live now… to have the courage to confront each day." },
+    { author: "— George Michael", quotes: " You’ll never find peace of mind until you listen to your heart." },
+    { author: " —Dalai Lama", quotes: "Do not let the behavior of others destroy your inner peace." },
+    { author: " —Eckhart Tolle ", quotes: "You find peace not by rearranging the circumstances of your life, but by realizing who you are at the deepest level." },
+    { author: " —John F. Kennedy", quotes: " Peace is a daily, a weekly, a monthly process, gradually changing opinions, slowly eroding old barriers, quietly building new structures." },
+    { author: " — Linda Evans", quotes: " If there’s no inner peace, people can’t give it to you. The husband can’t give it to you. Your children can’t give it to you. You have to give it to you." },
+    { author: " — Haile Selassie", quotes: " Peace is a day-to-day problem, the product of a multitude of events and judgments. Peace is not an ‘is,’ it is a ‘becoming.’" },
+    { author: " — Eleanor Roosevelt", quotes: " It isn’t enough to talk about peace. One must believe in it. And it isn’t enough to believe in it. One must work at it." },
+    { author: " — Ralph Waldo Emerson", quotes: " Nobody can bring you peace but yourself." },
+    { author: " — Norman Vincent Peale", quotes: " The life of inner peace, being harmonious and without stress, is the easiest type of existence." }
+    ],
+
+
+    'Melancholy': [{ author: "— Daniel Keyes", quotes: "I don’t know what’s worse: to not know what you are and be happy, or to become what you’ve always wanted to be, and feel alone." },
+    { author: "— Soren Kierkegaard", quotes: " What if everything in the world were a misunderstanding, what if laughter were really tears?" },
+    { author: " —Susan Sontag", quotes: "Depression is melancholy minus its charms." },
+    { author: " —P.G. Wodehouse", quotes: "A melancholy-looking man, he had the appearance of one who has searched for the leak in life's gas-pipe with a lighted candle." },
+    { author: " —Percy Bysshe Shelley", quotes: " A poet is a nightingale who sits in darkness and sings to cheer its own solitude with sweet sounds." },
+    { author: " — Sylvia Plath", quotes: " I wish to cry. Yet, I laugh, and my lipstick leaves a red stain like a bloody crescent moon on the top of the beer can." },
+    { author: " — Hermann Hesse", quotes: " I began to understand that suffering and disappointments and melancholy are there not to vex us or cheapen us or deprive us of our dignity but to mature and transfigure us." },
+    { author: " — Tim Winton", quotes: " It’s how I fill the time when nothing’s happening. Thinking too much, flirting with melancholy." },
+    { author: " — William Shakespeare", quotes: " There's little of the melancholy element in her, my lord: she is never sad but when she sleeps; and not ever sad then; for I have heard my daughter say, she hath often dreamt of unhappiness, and waked herself with laughing." },
+    { author: " — Oscar Wilde ", quotes: "The gods are strange. It is not our vices only they make instruments to scourge us. They bring us to ruin through what in us is good, gentle, humane, loving" }
+    ]
+
+
+}
+
+
+const kingdoms = {
 
     'senate': {
-name:'Senate',
-color:'#f1c40f',
-ship:'<img src="Recursos/css/imagenes/SVG/Planets/senate-ship.svg" width="2"></img>',
-logo:'<i class="fab fa-galactic-senate"></i>',
-NumberShips:0,
-planets:0,
-Army:0,
-Inhabitants:0},
-    
-
-'republic':{
-name:'Republic',
-color:'#e74c3c',
-ship:'<img src="Recursos/css/imagenes/SVG/Planets/spaceship.svg" width="2"></img>',
-logo:' <i class="fab fa-empire"></i>',
-NumberShips:0,
-planets:0,
-Army:0,
-Inhabitants:0}, 
-
-'gulp':{
-name:'Gulp',
-color: '#2ecc71',
-ship:'<img src="Recursos/css/imagenes/SVG/Planets/rocket.svg" width="2">',
-logo:'<i class="fab fa-bitcoin"></i>',
-NumberShips:0,
-planets:0,
-Army:0,
-Inhabitants:0},  
-
-'humans':{
-name:'Humans',
-color: '#3498db',
-ship:'<img src="Recursos/css/imagenes/SVG/Planets/battleship1.svg" width="2">',
-logo:'<i class="fab fa-wolf-pack-battalion"></i>',
-NumberShips:0,
-planets:0,
-Army:0,
-Inhabitants:0},
+        name: 'Senate',
+        color: '#f1c40f',
+        ship: '<img src="Recursos/css/imagenes/SVG/Planets/senate-ship.svg" width="2"></img>',
+        logo: '<i class="fab fa-galactic-senate"></i>',
+        ships: 0,
+        planets: 0,
+        Army: 0,
+        Inhabitants: 0
+    },
 
 
-    }
+    'republic': {
+        name: 'Republic',
+        color: '#e74c3c',
+        ship: '<img src="Recursos/css/imagenes/SVG/Planets/spaceship.svg" width="2"></img>',
+        logo: ' <i class="fab fa-empire"></i>',
+        ships: 0,
+        planets: 0,
+        Army: 0,
+        Inhabitants: 0
+    },
 
-    const planetas = [
-'<img src="Recursos/css/imagenes/SVG/Planets/planet1.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet2.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet3.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet4.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet5.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet6.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet7.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet8.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet9.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet10.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet11.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet12.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet13.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet14.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet15.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet16.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet17.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet18.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet19.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet20.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet21.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet22.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet23.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet24.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet25.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet26.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet27.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet28.svg" width="2">',
-'<img src="Recursos/css/imagenes/SVG/Planets/planet29.svg" width="2">']
+    'gulp': {
+        name: 'Gulp',
+        color: '#2ecc71',
+        ship: '<img src="Recursos/css/imagenes/SVG/Planets/rocket.svg" width="2">',
+        logo: '<i class="fab fa-bitcoin"></i>',
+        ships: 0,
+        planets: 0,
+        Army: 0,
+        Inhabitants: 0
+    },
+
+    'humans': {
+        name: 'Humans',
+        color: '#3498db',
+        ship: '<img src="Recursos/css/imagenes/SVG/Planets/battleship1.svg" width="2">',
+        logo: '<i class="fab fa-wolf-pack-battalion"></i>',
+        ships: 0,
+        planets: 0,
+        Army: 0,
+        Inhabitants: 0
+    },
+
+
+}
+
+
+const GLOBAL_STATISTICS = {
+    'planets': 0,
+    'ships': 0,
+    'Army': 0,
+    'Inhabitants': 0,
+}
+
+const planetas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
+
+const battleShips = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
